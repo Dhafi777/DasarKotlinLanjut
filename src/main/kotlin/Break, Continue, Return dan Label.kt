b36cbd490a356fdf73fa1ce184024333999761d4
Dhafi777 <ininama.404@gmail.com>
2024-09-14 17:29:03 +0700

    Add Break, Continue, Return dan Label.kt

--- src/main/kotlin/Break, Continue, Return dan Label.kt
+++ src/main/kotlin/Break, Continue, Return dan Label.kt
@@ -1,10 +1,16 @@
 fun main(args: Array<String>) {
+    returnLoop()
+}
+fun returnLoop(){
     var x:Int
-    println("Example of Break and Continue in For-Loop")
+    println("Example of return in For-Loop")
     for(x in 1..10) {
-        if(x == 7) break
-        if(x == 3) continue
+        if(x < 5) {
+            print("${x - 1} ")
+        }else {
+            return
+        }
         print("$x ")
     }
-    print('\n')
+    print("Tidak akan pernah dieksekusi")
 }
